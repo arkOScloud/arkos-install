fc58625cf47cff7e0c9ff7f79f42f201e909b833
jacook <jacob@jcook.cc>
2013-06-16 18:46:48 -0400

    make a proper python package

---
+++ Linux/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='arkos-install',
+    version='0.2',
+    author='Jacob Cook',
+    author_email='jacob@citizenweb.is',
+    packages=['arkos-install'],
+    url='http://arkos.io/docs/getting-started',
+    description='Installs the latest arkOS image to an SD card',
+)
