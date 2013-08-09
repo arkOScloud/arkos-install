ad99874f0aebca88b6f6eb393f5efdf798d7436c
jacook <jacob@jcook.cc>
2013-08-09 19:23:31 -0400

    fork Linux version for OSX

---
+++ OSX/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+# arkOS Installer for OSX
+
+setup(
+    name='arkos-install',
+    version='0.3',
+    author='Jacob Cook',
+    author_email='jacob@citizenweb.is',
+    packages=['arkos-install'],
+    url='http://arkos.io/docs/getting-started',
+    description='Installs the latest arkOS image to an SD card',
+)
